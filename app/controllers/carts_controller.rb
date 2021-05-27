3473c0ee4d0fdc5f2e6f928206b08ba09be1956d
Jessie-p05 <jxpei05@gmail.com>
2021-05-27 10:40:37 -0600

    move the logic to the backend

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,11 @@
 class CartsController < ApplicationController
-
+  
   def show
-    # if enhanced_cart.length == 0
-    #   render 'empty'
-    # else
-    #   # render 'show'
-    # end
+    @cartCount = cart.length
+    if @cartCount == 0
+      render :action => "special_show"
+    end
   end
-
-
 
   def add_item
     product_id = params[:product_id].to_s
