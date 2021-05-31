13e38103184ba8dc078b408d2afee9ea24b1fd81
Jessie-p05 <jxpei05@gmail.com>
2021-05-30 23:29:42 -0600

    correct the name form user to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def new
   end
   
@@ -14,6 +14,6 @@ class UserController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
