f3a05c281afbdc346a311942eab2457b1a75939f
Jessie-p05 <jxpei05@gmail.com>
2021-06-01 00:03:47 -0600

    add validates and a authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base 
-  has_secure_password 
+  has_secure_password
+
+  validates :email, :uniqueness => { :case_sensitive => false }, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true,length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email,password)
+    user = User.find_by("LOWER(email)= ?",email.strip.downcase)
+   if user && user.authenticate(password)
+    user
+   else
+    nil
+   end
+    
+  end
 end
