f454c548f1c1d75ff30e05644280c519a306b827
Jessie-p05 <jxpei05@gmail.com>
2021-05-31 20:19:45 -0600

    add rspect test to the product controller

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before do
+      @product = Product.new({:name => 'chair',:price => 100, :quantity => 20, :category => Category.new({:name => 'furniture'})})
+      @product.save!
+    end
+
+    it "should save sucessfully" do
+      expect(@product).to be_present
+    end
+
+    it "should have a name present" do
+      expect(@product.name).to be_present
+    end
+    it "should have a price present" do
+      expect(@product.price).to be_present
+    end
+    it "should have a quantity present" do
+      expect(@product.quantity).to be_present
+    end
+    it "should have a category present" do
+      expect(@product.category).to be_present
+    end
+
+    
+  end
+end
